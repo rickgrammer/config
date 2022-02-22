858662ffff683eca908047e40c818649c2128b98
Ashfaq Rahman <ashrmzn22@gmail.com>
2022-02-23 02:40:00 +0530

    ---

--- _git-tracker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ `git status --porcelain` ]]; then
-    echo "changes found in $(pwd)" >> /tmp/_git-tracker.log
-    git add -A
-    git commit -m 'automated'
-    git push origin master
-    echo "pushed changes to master for $(pwd)" >> /tmp/_git-tracker.log
-fi
-
