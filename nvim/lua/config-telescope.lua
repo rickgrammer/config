acbaeabe2c2b728dd864ba081b87be8b7990f957
ashfaq rahman <ashrmzn22@gmail.com>
2022-07-06 23:27:35 +0530

    automated

--- nvim/lua/config-telescope.lua
+++ nvim/lua/config-telescope.lua
@@ -5,8 +5,8 @@
 -- }
 local M = {}
 M.project_files = function()
-  local opts = {} -- define here if you want to define something
-  local ok = pcall(require"telescope.builtin".git_files({show_untracked=true}), opts)
+  local opts = {show_untracked = true} -- define here if you want to define something
+  local ok = pcall(require"telescope.builtin".git_files, opts)
   if not ok then require"telescope.builtin".find_files(opts) end
 end
 
