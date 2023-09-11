26c980ce9f1e0617026e5aaedb2b5925caea7407
Ashfaq Rahman <ashrmzn22@gmail.com>
2023-09-11 20:07:08 +0530

    s

--- opacity_toggler.sh
+++ opacity_toggler.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-is_transparent=$(cat ~/.config/alacritty/alacritty.yml | grep 'opacity: 0.85')
+#!/usr/bin/bash
+is_transparent=$(cat ~/.config/alacritty/alacritty.toml | grep "opacity = 0.85")
 if [[ $is_transparent ]]
 then
-    sed -i 's/opacity: 0.85/opacity: 1.00/' ~/.config/alacritty/alacritty.yml
+    sed -i 's/opacity = 0.85/opacity = 1.00/' ~/.config/alacritty/alacritty.toml
 else
-    sed -i 's/opacity: 1.0/opacity: 0.85/' ~/.config/alacritty/alacritty.yml
+    sed -i 's/opacity = 1.0/opacity = 0.85/' ~/.config/alacritty/alacritty.toml
 fi
